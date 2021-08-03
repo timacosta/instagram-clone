39bc8fa674d5045258f0445b8b0b19d35eb722fc
Tim Acosta <timacosta06@gmail.com>
2021-08-04 00:41:50 +0100

    room storage configured

--- app/src/main/java/io/keepcoding/instagram_clone/gallery/GalleryRepositoryImpl.kt
+++ app/src/main/java/io/keepcoding/instagram_clone/gallery/GalleryRepositoryImpl.kt
@@ -1,17 +1,29 @@
 package io.keepcoding.instagram_clone.gallery
 
-import android.net.Network
-import io.keepcoding.instagram_clone.GalleryViewModel
 import io.keepcoding.instagram_clone.gallery.Gallery.Image
 import io.keepcoding.instagram_clone.network.ImgurApi
 import io.keepcoding.instagram_clone.network.NetworkGallery
+import io.keepcoding.instagram_clone.room.ImageDAO
+import io.keepcoding.instagram_clone.room.RoomImage
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import timber.log.Timber
 
-class GalleryRepositoryImpl(private val imgurApi: ImgurApi): GalleryRepository {
+class GalleryRepositoryImpl(
+    private val imgurApi: ImgurApi,
+    private val imageDAO: ImageDAO
+    ): GalleryRepository {
+
     override suspend fun getHotGallery() =
         withContext(Dispatchers.IO) {
-            imgurApi.getHotGallery().toDomain()
+            try {
+                imgurApi.getHotGallery().toDomain().also { gallery ->
+                    imageDAO.insertImages(gallery.toRoom())
+                }
+            } catch (e: Exception) {
+                Timber.e(e)
+                imageDAO.getImages(RoomImage.ImageType.HOT).toDomain()
+            }
         }
 
     override suspend fun getTopGallery() =
@@ -42,5 +54,32 @@ class GalleryRepositoryImpl(private val imgurApi: ImgurApi): GalleryRepository {
         return Gallery(images)
     }
 
+    private fun List<RoomImage>.toDomain(): Gallery {
+        val images = map { roomImage ->
+            Image(
+                id = roomImage.id ,
+                title = roomImage.title,
+                url = roomImage.url,
+                likes = roomImage.likes,
+                datetime = roomImage.datetime,
+                author = roomImage.author
+            )
+        }
+        return Gallery(images)
+    }
+
+    private fun Gallery.toRoom(imageType: RoomImage.ImageType): List<RoomImage> {
+        return images.map { image ->
+            RoomImage(
+                id = image.id,
+                title = image.title,
+                url = image.url,
+                likes = image.likes,
+                datetime = image.datetime,
+                author = image.author,
+                type = imageType
+            )
+        }
+    }
 
 }
--- app/src/main/java/io/keepcoding/instagram_clone/room/ImageDAO.kt
+++ app/src/main/java/io/keepcoding/instagram_clone/room/ImageDAO.kt
@@ -1,12 +1,18 @@
 package io.keepcoding.instagram_clone.room
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy.REPLACE
 import androidx.room.Query
+import io.keepcoding.instagram_clone.room.RoomImage.ImageType
 
 @Dao
 interface ImageDAO {
 
     @Query("SELECT * FROM images WHERE type = :type")
-    fun getImages(type: RoomImage.ImageType): List<RoomImage>
+    fun getImages(type: ImageType): List<RoomImage>
+
+    @Insert(onConflict = REPLACE)
+    fun insertImages(imagesList: List<RoomImage>)
 
 }
